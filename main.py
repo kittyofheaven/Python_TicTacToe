ab981547a2845a31ee71345976a6c0241b2ae353
shironeko <71081063+kittyofheaven@users.noreply.github.com>
2021-10-05 07:24:09 +0000

    finalize the game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # python tic tac toe by shironeko
 import random
-import re
 import time
 from os import system, name
 
@@ -22,8 +21,10 @@ dot_list = ['0','1','2','3','4','5','6','7','8',]
 dot_choosen_list = []
 still_playing = True
 
-def tictactoe(player_now) :
-
+def tictactoe() :
+  player_now = starting_player
+  global still_playing
+  
   while still_playing == True :
 
     print('| '+dot_list[0]+' | '+dot_list[1]+' | '+dot_list[2] + ' |')
@@ -39,11 +40,16 @@ def tictactoe(player_now) :
       dot_list[int(number_choosen)] = player_now
       dot_choosen_list.append(int(number_choosen))
       clear()
-      
-      if player_now == starting_player :
-        tictactoe(next_player)
+
+      if dot_list[0] == dot_list[1] == dot_list[2] or dot_list[3] == dot_list[4] == dot_list[5] or dot_list[6] == dot_list[7] == dot_list[8] or dot_list[0] == dot_list[3] == dot_list[6] or dot_list[1] == dot_list[4] == dot_list[7] or dot_list[2] == dot_list[5] == dot_list[8] or dot_list[0] == dot_list[4] == dot_list[8] or dot_list[2] == dot_list[4] == dot_list[6] :
+    
+        still_playing = False
+    
       else :
-        tictactoe(starting_player)
+        if player_now == starting_player :
+          player_now = next_player
+        else :
+          player_now = starting_player
 
     elif int(number_choosen) <=8 and int(number_choosen) >= 0 and int(number_choosen) in (dot_choosen_list) :
 
@@ -54,7 +60,17 @@ def tictactoe(player_now) :
     else :
       print('ur input is invalid')
       exit()
+  
+  if dot_list[0] == dot_list[1] == dot_list[2] == starting_player or dot_list[3] == dot_list[4] == dot_list[5] == starting_player or dot_list[6] == dot_list[7] == dot_list[8] == starting_player or dot_list[0] == dot_list[3] == dot_list[6] == starting_player or dot_list[1] == dot_list[4] == dot_list[7] == starting_player or dot_list[2] == dot_list[5] == dot_list[8] == starting_player or dot_list[0] == dot_list[4] == dot_list[8] == starting_player or dot_list[2] == dot_list[4] == dot_list[6] == starting_player :
 
+    print(starting_player + " WIN !!!")
+
+  elif dot_list[0] == dot_list[1] == dot_list[2] == starting_player or dot_list[3] == dot_list[4] == dot_list[5] == next_player or dot_list[6] == dot_list[7] == dot_list[8] == next_player or dot_list[0] == dot_list[3] == dot_list[6] == next_player or dot_list[1] == dot_list[4] == dot_list[7] == next_player or dot_list[2] == dot_list[5] == dot_list[8] == next_player or dot_list[0] == dot_list[4] == dot_list[8] == next_player or dot_list[2] == dot_list[4] == dot_list[6] == next_player :
+
+    print(next_player + " WIN !!!")
+
+  else :
+    pass
     
 
-tictactoe(starting_player)
+tictactoe()
